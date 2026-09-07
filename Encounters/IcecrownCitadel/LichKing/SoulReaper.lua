5ce66099831e5294ff40a5d69f9390cb7e19c18c
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-07 14:32:02 +0300

    Add Soul Reaper plan data

---
+++ Encounters/IcecrownCitadel/LichKing/SoulReaper.lua
@@ -0,0 +1,26 @@
+local _, ns = ...
+ns = ns or {}
+
+ns.LichKingSoulReaper = {
+    Phase2 = {
+        [1] = "core_pair",
+        [2] = "ibf_solo",
+        [3] = "core_pair",
+        [4] = "remaining_core_trinket",
+        [5] = "core_pair",
+        [6] = "ibf_solo",
+        [7] = "core_pair",
+        [8] = "remaining_core_pain",
+    },
+
+    Phase3 = {
+        [1] = "core_pair_ibf",
+        [2] = "remaining_core_trinket",
+        [3] = "core_pair",
+        [4] = "ibf_solo",
+        [5] = "core_pair",
+        [6] = "remaining_core_pain",
+        [7] = "ibf_solo",
+        [8] = "core_pair",
+    },
+}
