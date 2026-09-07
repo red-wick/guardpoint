9f8efb8631e0a5c1e9d217092060ccedaa47250c
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-07 17:34:42 +0300

    Prepare Blood DK module namespace

--- Classes/DeathKnight/Blood.lua
+++ Classes/DeathKnight/Blood.lua
@@ -1,23 +1,25 @@
-local _, ns = ...
+-- Guardpoint Blood DK resources
+local NS = _G.Guardpoint or {}
+_G.Guardpoint = NS
 
-ns.BloodDK = {
+NS.BloodDK = NS.BloodDK or {
     Class = "DEATHKNIGHT",
 
     Spells = {
-        BloodTap = 45529,
-        VampiricBlood = 55233,
-        AntiMagicShell = 48707,
-        IceboundFortitude = 48792,
-        ArmyOfDead = 42650,
-        PainSuppression = 33206,
-        HandOfSacrifice = 6940,
+        TAP = 45529,
+        VB = 55233,
+        AMS = 48707,
+        IBF = 48792,
+        ARMY = 42650,
+        PAIN = 33206,
+        SAC = 6940,
     },
 
     Items = {
-        FangNormal = 50361,
-        FangHeroic = 50364,
-        SatrinaNormal = 47080,
-        SatrinaHeroic = 47088,
-        Key = 50356,
+        FANG_N = 50361,
+        FANG_H = 50364,
+        SATRINA_N = 47080,
+        SATRINA_H = 47088,
+        KEY = 50356,
     },
 }
