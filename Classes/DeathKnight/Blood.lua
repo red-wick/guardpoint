ad25520d992bf4bf0f37e8168678372ad3faa890
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-07 16:35:11 +0300

    Make Blood DK a standalone class module

--- Classes/DeathKnight/Blood.lua
+++ Classes/DeathKnight/Blood.lua
@@ -1,23 +1,41 @@
-local _, ns = ...
+-- Guardpoint Blood Death Knight module
+local NS = _G.Guardpoint
 
-ns.BloodDK = {
-    Class = "DEATHKNIGHT",
+NS.BloodDK = NS.BloodDK or {}
+local DK = NS.BloodDK
 
-    Spells = {
-        BloodTap = 45529,
-        VampiricBlood = 55233,
-        AntiMagicShell = 48707,
-        IceboundFortitude = 48792,
-        ArmyOfDead = 42650,
-        PainSuppression = 33206,
-        HandOfSacrifice = 6940,
-    },
+DK.Class = "DEATHKNIGHT"
 
-    Items = {
-        FangNormal = 50361,
-        FangHeroic = 50364,
-        SatrinaNormal = 47080,
-        SatrinaHeroic = 47088,
-        Key = 50356,
-    },
+DK.Spells = {
+    BloodTap = 45529,
+    VampiricBlood = 55233,
+    AntiMagicShell = 48707,
+    IceboundFortitude = 48792,
+    ArmyOfDead = 42650,
+    PainSuppression = 33206,
+    HandOfSacrifice = 6940,
 }
+
+DK.Items = {
+    FangNormal = 50361,
+    FangHeroic = 50364,
+    SatrinaNormal = 47080,
+    SatrinaHeroic = 47088,
+    Key = 50356,
+}
+
+DK.Actions = {
+    tap = DK.Spells.BloodTap,
+    vb = DK.Spells.VampiricBlood,
+    ams = DK.Spells.AntiMagicShell,
+    ibf = DK.Spells.IceboundFortitude,
+    army = DK.Spells.ArmyOfDead,
+    pain = DK.Spells.PainSuppression,
+    sac = DK.Spells.HandOfSacrifice,
+}
+
+DK.Items.FANG_N = DK.Items.FangNormal
+DK.Items.FANG_H = DK.Items.FangHeroic
+DK.Items.SATRINA_N = DK.Items.SatrinaNormal
+DK.Items.SATRINA_H = DK.Items.SatrinaHeroic
+DK.Items.KEY = DK.Items.Key
