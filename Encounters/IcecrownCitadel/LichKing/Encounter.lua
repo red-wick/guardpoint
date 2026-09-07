b281e0a0917dfe601707d186d772738495732c87
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-07 13:33:36 +0300

    Add Lich King encounter data

---
+++ Encounters/IcecrownCitadel/LichKing/Encounter.lua
@@ -0,0 +1,24 @@
+local _, ns = ...
+
+ns.LichKing = {
+    BossID = 36597,
+    MapID = 631,
+
+    ReaperIDs = {
+        [69409] = true,
+        [73797] = true,
+        [73798] = true,
+        [73799] = true,
+    },
+
+    QuakeID = 72262,
+
+    Timing = {
+        P2First = 32.0,
+        P3First = 37.5,
+        NextReaper = 34.0,
+        ReaperDuration = 5.1,
+        Prewarn = 8.0,
+        GlowLead = 5.0,
+    },
+}
