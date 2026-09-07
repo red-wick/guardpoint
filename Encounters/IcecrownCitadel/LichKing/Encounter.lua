7acc605f52f2ed14d3695da15c264b131214cdf8
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-07 18:18:32 +0300

    Expose encounter spell checks

--- Encounters/IcecrownCitadel/LichKing/Encounter.lua
+++ Encounters/IcecrownCitadel/LichKing/Encounter.lua
@@ -43,6 +43,14 @@ function LichKing.IsBossGUID(guid)
     return LichKing.CreatureEntryFromGUID(guid) == LichKing.BossID
 end
 
+function LichKing.IsReaperSpell(spellID)
+    return spellID and LichKing.ReaperIDs[spellID] == true
+end
+
+function LichKing.IsQuakeSpell(spellID)
+    return spellID and spellID == LichKing.QuakeID
+end
+
 function LichKing.FindUnit()
     local units={"boss1","boss2","boss3","boss4","target","focus","mouseover"}
     for i=1,#units do
