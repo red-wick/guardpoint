ce3b476645196763d8d444e47623cefad8010120
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:57:05 +0300

    Add item data foundation

--- Data/Items.lua
+++ Data/Items.lua
@@ -1,1 +1,9 @@
--- Item definitions placeholder.
+Guardpoint.Data.Items = Guardpoint.Data.Items or {}
+
+local Items = Guardpoint.Data.Items
+
+Items.Common = {
+    Healthstone = 47875,
+    RunicHealingPotion = 33447,
+    IndestructiblePotion = 40093,
+}
