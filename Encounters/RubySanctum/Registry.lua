7093710a3c4ab7fe3342c5b22257803bc2e94616
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 14:02:20 +0300

    Add Ruby Sanctum registry

--- Encounters/RubySanctum/Registry.lua
+++ Encounters/RubySanctum/Registry.lua
@@ -1,1 +1,21 @@
--- Ruby Sanctum encounter registry placeholder.
+Guardpoint.Encounters.RubySanctum = Guardpoint.Encounters.RubySanctum or {}
+
+local RS = Guardpoint.Encounters.RubySanctum
+
+RS.instanceID = 724
+RS.name = "Ruby Sanctum"
+RS.encounters = RS.encounters or {}
+
+function RS:Register(encounterID, encounterData)
+    if type(encounterID) ~= "number" or type(encounterData) ~= "table" then
+        return
+    end
+
+    self.encounters[encounterID] = encounterData
+end
+
+function RS:Get(encounterID)
+    return self.encounters[encounterID]
+end
+
+Guardpoint.Encounters.Registry:Register(RS.instanceID, RS)
