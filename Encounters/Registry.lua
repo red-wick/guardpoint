058c4dee02158c72de7c1f05013c581530f34745
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 14:02:06 +0300

    Add encounter registry

--- Encounters/Registry.lua
+++ Encounters/Registry.lua
@@ -1,1 +1,21 @@
--- Encounter registry placeholder.
+Guardpoint.Encounters = Guardpoint.Encounters or {}
+Guardpoint.Encounters.Registry = Guardpoint.Encounters.Registry or {}
+
+local Registry = Guardpoint.Encounters.Registry
+
+function Registry:Register(instanceID, encounterData)
+    if type(instanceID) ~= "number" or type(encounterData) ~= "table" then
+        return
+    end
+
+    self[instanceID] = encounterData
+end
+
+function Registry:Get(instanceID)
+    return self[instanceID]
+end
+
+function Registry:GetCurrent()
+    local instanceID = select(8, GetInstanceInfo())
+    return self:Get(instanceID)
+end
