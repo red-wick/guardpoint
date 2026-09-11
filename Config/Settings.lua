66d1a478eee723d1f7652cf0bd675b6b8865fa02
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 15:23:23 +0300

    Initialize persistent configuration

--- Config/Settings.lua
+++ Config/Settings.lua
@@ -1,1 +1,34 @@
--- User settings placeholder.
+Guardpoint.Config = Guardpoint.Config or {}
+
+local Config = Guardpoint.Config
+
+function Config:Initialize()
+    GuardpointDB = GuardpointDB or {}
+
+    for key, defaultValue in pairs(self.Defaults) do
+        if GuardpointDB[key] == nil then
+            GuardpointDB[key] = defaultValue
+        end
+    end
+
+    self.values = GuardpointDB
+end
+
+function Config:Get(key)
+    if not self.values then
+        return nil
+    end
+
+    return self.values[key]
+end
+
+function Config:Set(key, value)
+    if not self.values or self.Defaults[key] == nil then
+        return false
+    end
+
+    self.values[key] = value
+    return true
+end
+
+Config:Initialize()
