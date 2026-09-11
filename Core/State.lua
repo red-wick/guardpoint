d135e8ebd415864d310ca9ee83a763a8cdd951f2
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 14:25:21 +0300

    Track current instance in state

--- Core/State.lua
+++ Core/State.lua
@@ -1,12 +1,24 @@
 Guardpoint.State = Guardpoint.State or {
     playerClass = nil,
     inCombat = false,
+    instance = nil,
     encounter = nil,
 }
 
 function Guardpoint.State:RefreshPlayer()
     local class = UnitClass("player")
     self.playerClass = class
+end
+
+function Guardpoint.State:RefreshInstance()
+    if not Guardpoint.Encounters or not Guardpoint.Encounters.Registry then
+        self.instance = nil
+        self.encounter = nil
+        return
+    end
+
+    self.instance = Guardpoint.Encounters.Registry:GetCurrent()
+    self.encounter = nil
 end
 
 function Guardpoint.State:SetCombat(active)
