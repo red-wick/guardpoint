05497bc614be56e3001e9f7128dca722c76c827a
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:33:04 +0300

    Add GuardPoint slash commands

--- Runtime/Commands.lua
+++ Runtime/Commands.lua
@@ -1,1 +1,20 @@
--- Slash command handling placeholder.
+SLASH_GUARDPOINT1 = "/guardpoint"
+SLASH_GUARDPOINT2 = "/gp"
+
+SlashCmdList.GUARDPOINT = function(message)
+    local command = string.lower(message or "")
+
+    if command == "debug" then
+        local state = Guardpoint.State
+        Guardpoint:Print("version " .. Guardpoint.VERSION)
+        Guardpoint:Print("class: " .. tostring(state.playerClass))
+        Guardpoint:Print("combat: " .. tostring(state.inCombat))
+        Guardpoint:Print("encounter: " .. tostring(state.encounter))
+    elseif command == "reset" then
+        Guardpoint.State:SetCombat(false)
+        Guardpoint.State:SetEncounter(nil)
+        Guardpoint:Print("state reset")
+    else
+        Guardpoint:Print("v" .. Guardpoint.VERSION .. " | /gp debug | /gp reset")
+    end
+end
