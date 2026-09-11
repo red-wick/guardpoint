e2d0bf2c4caf5b39417904e4ac6b6d6468e7adac
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 16:55:06 +0300

    Remove direct renderer runtime dependency

--- Runtime/Events.lua
+++ Runtime/Events.lua
@@ -18,14 +18,11 @@ frame:SetScript("OnEvent", function(self, event, ...)
         Guardpoint.State:RefreshPlayer()
         Guardpoint.State:RefreshInstance()
         Guardpoint.EventBus:Fire(event, ...)
-        Guardpoint.UI.Renderer:Refresh()
     elseif event == "PLAYER_REGEN_DISABLED" then
         Guardpoint.State:SetCombat(true)
         Guardpoint.EventBus:Fire("COMBAT_START", ...)
-        Guardpoint.UI.Renderer:Refresh()
     elseif event == "PLAYER_REGEN_ENABLED" then
         Guardpoint.State:SetCombat(false)
         Guardpoint.EventBus:Fire("COMBAT_END", ...)
-        Guardpoint.UI.Renderer:Refresh()
     end
 end)
