b8559abf85070d3acc3ed6845d6847d2b952d9d1
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 18:30:34 +0300

    Add shared type utilities

--- Core/Utils.lua
+++ Core/Utils.lua
@@ -1,1 +1,19 @@
--- Shared utility functions placeholder.
+Guardpoint.Utils = Guardpoint.Utils or {}
+
+local Utils = Guardpoint.Utils
+
+function Utils.IsNumber(value)
+    return type(value) == "number"
+end
+
+function Utils.IsString(value)
+    return type(value) == "string"
+end
+
+function Utils.IsTable(value)
+    return type(value) == "table"
+end
+
+function Utils.IsFunction(value)
+    return type(value) == "function"
+end
