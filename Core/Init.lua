2a4936bedd7f8f379908797db39a1fba1da575d5
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:32:34 +0300

    Implement core initialization

--- Core/Init.lua
+++ Core/Init.lua
@@ -1,2 +1,15 @@
--- Guardpoint core initialization.
--- Foundation only.
+Guardpoint = Guardpoint or {}
+
+Guardpoint.VERSION = "1.1.0"
+
+Guardpoint.Core = Guardpoint.Core or {}
+Guardpoint.Runtime = Guardpoint.Runtime or {}
+Guardpoint.UI = Guardpoint.UI or {}
+Guardpoint.Data = Guardpoint.Data or {}
+Guardpoint.Classes = Guardpoint.Classes or {}
+Guardpoint.Encounters = Guardpoint.Encounters or {}
+Guardpoint.Config = Guardpoint.Config or {}
+
+function Guardpoint:Print(message)
+    DEFAULT_CHAT_FRAME:AddMessage("|cff66ccffGuardPoint|r: " .. tostring(message))
+end
