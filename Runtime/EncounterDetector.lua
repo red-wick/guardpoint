2fffc00ea3b4888e929fcd3cd345e3aa5c308910
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 17:34:14 +0300

    Clear encounter reason after leaving instance

--- Runtime/EncounterDetector.lua
+++ Runtime/EncounterDetector.lua
@@ -157,6 +157,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_ENTERING_WORLD" then
         Detector:End("LEAVE")
         Detector:Refresh()
+        Guardpoint.State:SetEncounterEndReason(nil)
         return
     end
 
