83aa6b36ee47d79c9dbe12930bb4a52bd56467ec
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:56:57 +0300

    Add spell data foundation

--- Data/Spells.lua
+++ Data/Spells.lua
@@ -1,1 +1,36 @@
--- Spell definitions placeholder.
+Guardpoint.Data.Spells = Guardpoint.Data.Spells or {}
+
+local Spells = Guardpoint.Data.Spells
+
+Spells.Tank = {
+    DeathKnight = {
+        IceboundFortitude = 48792,
+        AntiMagicShell = 48707,
+        VampiricBlood = 55233,
+        BoneShield = 49222,
+    },
+    Warrior = {
+        ShieldWall = 871,
+        LastStand = 12975,
+        SpellReflection = 23920,
+    },
+    Paladin = {
+        DivineProtection = 498,
+        ArdentDefender = 31850,
+        HandOfSacrifice = 6940,
+    },
+    Druid = {
+        Barkskin = 22812,
+        SurvivalInstincts = 61336,
+        FrenziedRegeneration = 22842,
+    },
+}
+
+Spells.Common = {
+    Taunt = {
+        DeathKnight = 56222,
+        Warrior = 355,
+        Paladin = 62124,
+        Druid = 6795,
+    },
+}
