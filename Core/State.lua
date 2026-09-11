3123bf49d017f802b10e5aca30ba30178f39b116
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 14:59:37 +0300

    Track encounter start time

--- Core/State.lua
+++ Core/State.lua
@@ -3,6 +3,7 @@ Guardpoint.State = Guardpoint.State or {
     inCombat = false,
     instance = nil,
     encounter = nil,
+    encounterStartedAt = nil,
 }
 
 function Guardpoint.State:RefreshPlayer()
@@ -14,11 +15,13 @@ function Guardpoint.State:RefreshInstance()
     if not Guardpoint.Encounters or not Guardpoint.Encounters.Registry then
         self.instance = nil
         self.encounter = nil
+        self.encounterStartedAt = nil
         return
     end
 
     self.instance = Guardpoint.Encounters.Registry:GetCurrent()
     self.encounter = nil
+    self.encounterStartedAt = nil
 end
 
 function Guardpoint.State:SetCombat(active)
@@ -27,4 +30,5 @@ end
 
 function Guardpoint.State:SetEncounter(encounter)
     self.encounter = encounter
+    self.encounterStartedAt = encounter and GetTime() or nil
 end
