d23b19f2fbae39ab6821432b7f743eb844b0fb3b
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 15:34:46 +0300

    Create base UI panel

--- UI/Panel.lua
+++ UI/Panel.lua
@@ -1,1 +1,25 @@
--- Main mechanic panel placeholder.
+Guardpoint.UI = Guardpoint.UI or {}
+
+local UI = Guardpoint.UI
+
+UI.Panel = UI.Panel or CreateFrame("Frame", "GuardpointPanel", UI.Root)
+
+function UI.Panel:Initialize()
+    self:SetParent(UI.Root)
+    self:SetWidth(320)
+    self:SetHeight(80)
+    self:SetPoint("CENTER", UI.Root, "CENTER", 0, 0)
+    self:SetFrameLevel(UI.Root:GetFrameLevel() + 1)
+    UI.Styles:ApplyFrame(self)
+    self:Hide()
+end
+
+function UI.Panel:ShowPanel()
+    self:Show()
+end
+
+function UI.Panel:HidePanel()
+    self:Hide()
+end
+
+UI.Panel:Initialize()
