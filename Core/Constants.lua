d282dfe3229e1d785fd4ab9b71721da6a37101b1
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:32:39 +0300

    Define core constants

--- Core/Constants.lua
+++ Core/Constants.lua
@@ -1,1 +1,5 @@
--- Shared constants placeholder.
+Guardpoint.Constants = Guardpoint.Constants or {}
+
+Guardpoint.Constants.VERSION = "1.1.0"
+Guardpoint.Constants.SLASH_COMMAND = "/guardpoint"
+Guardpoint.Constants.SLASH_ALIAS = "/gp"
