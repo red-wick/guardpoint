ee1bf41834cb3eff3aeac2deabff36fbf2672b62
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 14:02:12 +0300

    Add Icecrown Citadel registry

--- Encounters/IcecrownCitadel/Registry.lua
+++ Encounters/IcecrownCitadel/Registry.lua
@@ -1,1 +1,21 @@
--- Icecrown Citadel encounter registry placeholder.
+Guardpoint.Encounters.IcecrownCitadel = Guardpoint.Encounters.IcecrownCitadel or {}
+
+local ICC = Guardpoint.Encounters.IcecrownCitadel
+
+ICC.instanceID = 631
+ICC.name = "Icecrown Citadel"
+ICC.encounters = ICC.encounters or {}
+
+function ICC:Register(encounterID, encounterData)
+    if type(encounterID) ~= "number" or type(encounterData) ~= "table" then
+        return
+    end
+
+    self.encounters[encounterID] = encounterData
+end
+
+function ICC:Get(encounterID)
+    return self.encounters[encounterID]
+end
+
+Guardpoint.Encounters.Registry:Register(ICC.instanceID, ICC)
