741397c77ef705a2cbf1c24b71f2092fa8b524cc
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:59:59 +0300

    Implement class registry

--- Classes/Registry.lua
+++ Classes/Registry.lua
@@ -1,1 +1,21 @@
--- Class registry placeholder.
+Guardpoint.Classes = Guardpoint.Classes or {}
+Guardpoint.Classes.Registry = Guardpoint.Classes.Registry or {}
+
+local Registry = Guardpoint.Classes.Registry
+
+function Registry:Register(classToken, classData)
+    if type(classToken) ~= "string" or type(classData) ~= "table" then
+        return
+    end
+
+    self[classToken] = classData
+end
+
+function Registry:Get(classToken)
+    return self[classToken]
+end
+
+function Registry:GetPlayerClass()
+    local classToken = select(2, UnitClass("player"))
+    return self:Get(classToken)
+end
