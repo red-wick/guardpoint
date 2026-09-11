deada990c8fba794f604846f933ceec11d2ca3e4
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:57:11 +0300

    Add icon data helpers

--- Data/Icons.lua
+++ Data/Icons.lua
@@ -1,1 +1,22 @@
--- Icon definitions placeholder.
+Guardpoint.Data.Icons = Guardpoint.Data.Icons or {}
+
+local Icons = Guardpoint.Data.Icons
+
+Icons.Default = "Interface\\Icons\\INV_Misc_QuestionMark"
+
+function Icons:GetSpell(spellID)
+    if type(spellID) ~= "number" then
+        return self.Default
+    end
+
+    local _, _, icon = GetSpellInfo(spellID)
+    return icon or self.Default
+end
+
+function Icons:GetItem(itemID)
+    if type(itemID) ~= "number" then
+        return self.Default
+    end
+
+    return GetItemIcon(itemID) or self.Default
+end
