e529f48f6d8c8846193826dfaf581ca5806c7419
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 13:32:51 +0300

    Implement internal event bus

--- Core/EventBus.lua
+++ Core/EventBus.lua
@@ -1,1 +1,23 @@
--- Internal event bus placeholder.
+Guardpoint.EventBus = Guardpoint.EventBus or {}
+
+local listeners = {}
+
+function Guardpoint.EventBus:Register(event, callback)
+    if type(callback) ~= "function" then
+        return
+    end
+
+    listeners[event] = listeners[event] or {}
+    table.insert(listeners[event], callback)
+end
+
+function Guardpoint.EventBus:Fire(event, ...)
+    local eventListeners = listeners[event]
+    if not eventListeners then
+        return
+    end
+
+    for _, callback in ipairs(eventListeners) do
+        callback(...)
+    end
+end
