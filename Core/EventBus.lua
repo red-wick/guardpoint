732d7fda31d7da0d326eb388c3b722d617249bb8
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 17:52:18 +0300

    Add EventBus listener removal

--- Core/EventBus.lua
+++ Core/EventBus.lua
@@ -3,12 +3,29 @@ Guardpoint.EventBus = Guardpoint.EventBus or {}
 local listeners = {}
 
 function Guardpoint.EventBus:Register(event, callback)
-    if type(callback) ~= "function" then
+    if type(event) ~= "string" or type(callback) ~= "function" then
         return
     end
 
     listeners[event] = listeners[event] or {}
     table.insert(listeners[event], callback)
+end
+
+function Guardpoint.EventBus:Unregister(event, callback)
+    local eventListeners = listeners[event]
+    if not eventListeners or type(callback) ~= "function" then
+        return
+    end
+
+    for index = table.getn(eventListeners), 1, -1 do
+        if eventListeners[index] == callback then
+            table.remove(eventListeners, index)
+        end
+    end
+
+    if table.getn(eventListeners) == 0 then
+        listeners[event] = nil
+    end
 end
 
 function Guardpoint.EventBus:Fire(event, ...)
