9ef21d0e8c74b75912c2604e384ac0f226330ba1
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-11 14:21:05 +0300

    Register Lord Marrowgar

--- Encounters/IcecrownCitadel/Bosses/LordMarrowgar.lua
+++ Encounters/IcecrownCitadel/Bosses/LordMarrowgar.lua
@@ -1,1 +1,6 @@
--- Lord Marrowgar encounter placeholder.
+local encounter = {
+    key = "LordMarrowgar",
+    name = "Lord Marrowgar",
+}
+
+Guardpoint.Encounters.IcecrownCitadel:Register("LordMarrowgar", encounter)
