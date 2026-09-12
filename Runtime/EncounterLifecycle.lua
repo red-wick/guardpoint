8491571ec5481bd366639cd9e551fb93eed3c922
gleboffhub <107622930+gleboffhub@users.noreply.github.com>
2026-09-12 14:33:25 +0300

    Keep encounter state ownership in detector

--- Runtime/EncounterLifecycle.lua
+++ Runtime/EncounterLifecycle.lua
@@ -12,10 +12,6 @@ Guardpoint.EventBus:Register("ENCOUNTER_START", function()
     CancelEncounterTasks()
 end)
 
-Guardpoint.EventBus:Register("ENCOUNTER_END", function(_, reason)
+Guardpoint.EventBus:Register("ENCOUNTER_END", function()
     CancelEncounterTasks()
-
-    if Guardpoint.State:GetEncounter() then
-        Guardpoint.State:SetEncounterEndReason(reason)
-    end
 end)
